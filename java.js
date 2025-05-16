43ff7c421ddca4a33a3f4c5d11b40871d1b03ccb
hitesh0806 <hiteshtg812@gmail.com>
2025-05-16 23:31:31 +0530

    prototyping and modifying the webpage

---
+++ java.js
@@ -0,0 +1,14 @@
+function road()
+{
+   window.location.href="road.html" 
+}
+
+function drainage()
+{
+   window.location.href="drainage.html" 
+}
+function streetlight()
+{
+    window.location.href="street light.html"
+}
+
